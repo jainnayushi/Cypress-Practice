5aace743044d5f7b3a4db7996786722775345410
Ayushi Jain <ayushi_jain3@persistent.com>
2022-12-20 19:52:14 +0530

    Commit

--- cypress/e2e/my-app.cy.js
+++ cypress/e2e/my-app.cy.js
@@ -4,46 +4,8 @@ describe("Notion tests", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
-
   it("Check Landing Page", () => {
     cy.contains("Notion");
-  });
-  it("Check Responsiveness", () => {
-    cy.contains("Notion");
-  });
-  it("Add Tasks", () => {
-    cy.contains("Task 1").should("not.exist");
-    cy.get('[data-input ="input"]').type("Task 1");
-    cy.get('[data-submit ="submit"]').click();
-    cy.contains("Task 1").should("exist");
-    cy.contains("item added to the list");
-
-    cy.contains("Task 2").should("not.exist");
-    cy.get('[data-input ="input"]').type("Task 2");
-    cy.get('[data-submit ="submit"]').click();
-    cy.contains("Task 2").should("exist");
-    cy.contains("item added to the list");
-
-    cy.contains("Task 3").should("not.exist");
-    cy.get('[data-input ="input"]').type("Task 3");
-    cy.get('[data-submit ="submit"]').click();
-    cy.contains("Task 3").should("exist");
-    cy.contains("item added to the list");
-
-    cy.contains("Task 1").should("exist");
-    cy.get('[data-edit ="Task 1"]').click();
-    cy.get('[data-input ="input"]').clear().type("Task 1 Updated");
-    cy.get('[data-submit ="submit"]').click();
-    cy.contains("Task 1 Updated").should("exist");
-    cy.contains("value changed");
-
-    cy.contains("Task 3").should("exist");
-    cy.get('[data-delete ="Task 3"]').click();
-    cy.contains("Task 3").should("not.exist");
-    cy.contains("item removed");
-
-    cy.get('[data-clear ="clear"]').click();
-    cy.contains("empty list");
   });
   it("Edit Task 1", () => {
     cy.contains("Task 1").should("not.exist");
@@ -118,5 +80,23 @@ describe("Notion tests", () => {
     cy.contains("empty list", { timeout: 2 * 1000 }).should("exist");
     cy.contains("empty list", { timeout: 4 * 1000 }).should("not.exist");
   });
+  it("Add Tasks", () => {
+    cy.contains("Task 1").should("not.exist");
+    cy.get('[data-input ="input"]').type("Task 1");
+    cy.get('[data-submit ="submit"]').click();
+    cy.contains("Task 1").should("exist");
+    cy.contains("item added to the list");
 
+    cy.contains("Task 2").should("not.exist");
+    cy.get('[data-input ="input"]').type("Task 2");
+    cy.get('[data-submit ="submit"]').click();
+    cy.contains("Task 2").should("exist");
+    cy.contains("item added to the list");
+
+    cy.contains("Task 3").should("not.exist");
+    cy.get('[data-input ="input"]').type("Task 3");
+    cy.get('[data-submit ="submit"]').click();
+    cy.contains("Task 3").should("exist");
+    cy.contains("item added to the list");
+  });
 });
